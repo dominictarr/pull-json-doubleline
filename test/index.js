724d3af3dfdab9062967368299e97d348f34d38f
Dominic Tarr <dominic.tarr@gmail.com>
2016-03-27 21:28:46 +1300

    test coverage for duplex streams

--- test/index.js
+++ test/index.js
@@ -1,7 +1,10 @@
 var JSONDL = require('../')
 var assert = require('assert')
 var pull = require('pull-stream')
+var pair = require('pull-pair')
+
 var input = [1,2, {okay: true}, "hello"]
+
 pull(
   pull.values(input),
   JSONDL.stringify(),
@@ -12,3 +15,18 @@ pull(
     console.log('passed')
   })
 )
+
+pull(
+  pull.values(input),
+  JSONDL.stringify(),
+  JSONDL(pair()),
+    pull.through(console.log),
+  JSONDL.parse(),
+  pull.collect(function (err, ary) {
+    assert.deepEqual(ary, input)
+    console.log('passed')
+  })
+)
+
+
+
