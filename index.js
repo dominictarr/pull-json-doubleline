40c86cc30ee4e25f81060f3621fc008f2936a37a
Dominic Tarr <dominic.tarr@gmail.com>
2016-03-27 21:24:27 +1300

    encode/decode a duplex stream,

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var Stringify = require('pull-stringify')
 var Split = require('pull-split')
 
-exports.stringify = function () {
+var p = require('is-pull-stream')
+
+function duplex (stream) {
+  return {
+    source: pull(stream.source, stringify()),
+    sink: pull(parse(), stream.sink)
+  }
+}
+
+function stringify () {
   return Stringify('', '\n', '\n\n', 2)
 }
-exports.parse = function () {
+
+function parse () {
   return Split('\n\n', JSON.parse)
 }
 
+exports = module.exports = function (stream) {
+  return (
+    p.isSource(stream) ? pull(stream, parse())
+  : p.isSink(stream)   ? pull(stringify(), stream)
+  :                      duplex(stream)
+  )
+}
 
-
-
-
+exports.stringify = stringify
+exports.parse = parse
